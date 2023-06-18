e5b4d351f40d551656223f573c3439f90c0839d6
v.goncharov <vgoncharov@hse.ru>
2023-06-18 14:42:14 +0300

    fix toString in Client

--- src/main/java/org/example/entities/Client.java
+++ src/main/java/org/example/entities/Client.java
@@ -1,25 +1,25 @@
 package org.example.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.Map;
 
 @AllArgsConstructor
 @Getter
 @Setter
-@ToString
 public class Client {
     private String name;
     private int balance; // баланс в долларах
     // балансы по ценным бумагам, ключ название бумаги, значение её количество
     private Map<String, Integer> papersMap;
+
     // если количество бумаг выросло, передать дельту с +, если уменьшилось, то с минус
     public void updatePapersCount(String key, Integer delta) {
         papersMap.put(key, papersMap.get(key) + delta);
     }
 
+    @Override
+    public String toString() {
+        return String.format("%s\t%d\t%s\t%s\t%s\t%s", name, balance, papersMap.get("A"), papersMap.get("B"), papersMap.get("C"), papersMap.get("D"));
+    }
 }
-
